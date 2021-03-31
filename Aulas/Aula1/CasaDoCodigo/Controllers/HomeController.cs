82e5ad84c0dba16ecdfd8f1d1b8932ad8b45a6ff
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-03-31 09:47:26 -0300

    A Página Carrossel

--- Aulas/Aula1/CasaDoCodigo/Controllers/HomeController.cs
+++ Aulas/Aula1/CasaDoCodigo/Controllers/HomeController.cs
@@ -10,26 +10,26 @@ namespace CasaDoCodigo.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index() // A funcao tem que chamar o nome da view Index.cshtml
         {
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult About() // A funcao tem que chamar o nome da view About.cshtml
         {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
-        public IActionResult Contact()
+        public IActionResult Contact() // A funcao tem que chamar o nome da view Contact.cshtml
         {
             ViewData["Message"] = "Your contact page.";
 
             return View();
         }
 
-        public IActionResult Error()
+        public IActionResult Error() // A funcao tem que chamar o nome da view Error.cshtml
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
