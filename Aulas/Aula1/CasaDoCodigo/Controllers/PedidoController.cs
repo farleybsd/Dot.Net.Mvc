41ac9c5fdaab9dc5656b61ab628e3520b39a0771
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-03-31 11:09:21 -0300

    Modificando a Rota Padrão do MVC

--- Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
+++ Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -8,7 +8,25 @@ namespace CasaDoCodigo.Controllers
 {
     public class PedidoController : Controller
     {
+        //http://localhost:56317/pedido/carrossel
         public IActionResult Carrossel()
+        {
+            return View();
+        }
+
+        //http://localhost:56317/pedido/carrinho
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+
+        //http://localhost:56317/pedido/cadastro
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+        //http://localhost:56317/pedido/resumo
+        public IActionResult Resumo()
         {
             return View();
         }
