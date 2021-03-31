418ceb1dd9b70670a1414e06a5c92b00e561965b
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-03-31 12:19:27 -0300

    Relacionamentos e Migrações

--- Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
+++ Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
@@ -18,6 +18,22 @@ namespace CasaDoCodigo
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Produto>().HasKey(p => p.Id);
+
+            //Relacionamento de um para muitos
+            modelBuilder.Entity<Pedido>().HasKey(p => p.Id);
+            modelBuilder.Entity<Pedido>().HasMany(p => p.Itens) // relacionamento de ida
+                                         .WithOne(p => p.Pedido); // relacionamento de volta com um pedido
+            //Relacionamento de um para Um
+            modelBuilder.Entity<Pedido>().HasOne(p=> p.Cadastro) // relacionamento de ida
+                                         .WithOne(p => p.Pedido).IsRequired(); // relacionamento de volta com um pedido
+            //Relacionamento ItemPedido
+            modelBuilder.Entity<ItemPedido>().HasKey(p => p.Id);
+            modelBuilder.Entity<ItemPedido>().HasOne(p => p.Pedido); // relacionamento de ida
+            modelBuilder.Entity<ItemPedido>().HasOne(p => p.Produto);
+
+            //Relacionamento Cadastro
+            modelBuilder.Entity<Cadastro>().HasKey(p => p.Id);
+            modelBuilder.Entity<Cadastro>().HasOne(p => p.Pedido);
         }
     }
 }
