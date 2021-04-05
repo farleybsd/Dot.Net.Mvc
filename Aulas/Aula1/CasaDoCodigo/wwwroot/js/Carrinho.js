a9c8197bfcb259e7a972522f30e61d0aff478a6e
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-04-05 20:23:32 -0300

    Organizando Código JavaScript com Classe

---
+++ Aulas/Aula1/CasaDoCodigo/wwwroot/js/Carrinho.js
@@ -0,0 +1,38 @@
+﻿class Carrinho
+{
+    clickIncremento(btn) {
+
+        let data = this.GetData(btn);
+        data.Quantidade++;
+        this.PostQuantidade(data);
+       
+    }
+
+    clickDecremento(btn) {
+        let data = this.GetData(btn);
+        data.Quantidade--;
+        this.PostQuantidade(data);
+    }
+
+    GetData(elemento) {
+        var linhaDoItem = $(elemento).parents('[item-id]'); // pegando elemento a cima do botao no DOM
+        var itemId = $(linhaDoItem).attr('item-id');
+        var novaQtd = $(linhaDoItem).find('input').val() // pegando elemento a abaixo do botao no DOM
+
+        return {
+            Id: itemId,
+            Quantidade: novaQtd
+        };
+    }
+
+    PostQuantidade(data) {
+        $.ajax({
+            url: '/pedido/UpdateQuantidade',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(data)
+        });
+    }
+}
+
+var carrinho = new Carrinho();
